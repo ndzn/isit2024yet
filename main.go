0faf9b4c63183090f5e5a67de9971ea4f0b65e66
ndzn <nikidurzynski@gmail.com>
2023-01-12 22:08:37 -0500

    ✨ Checked on date retuned

--- main.go
+++ main.go
@@ -15,11 +15,13 @@ func main() {
 		title := "Is it 2024 yet?"
 		color := "red"
 		message := "it isn't."
+		currentTime := time.Now().Format("2006-01-02 15:04:05")
 
 		if currentYear == 2024 {
 			title = "Is it 2024 yet?"
 			color = "green"
 			message = "It is!"
+			currentTime = time.Now().Format("2006-01-02 15:04:05")
 		}
 
 		html := fmt.Sprintf(`
@@ -41,14 +43,20 @@ func main() {
 						font-size: 50px;
 						margin: 50px 0;
 					}
+					h2 {
+						color: rgba(255, 255, 255, 0.2);
+						font-size: 10px;
+						margin: 0;
+					}
 				</style>
 			</head>
 			<body>
 				<h1>%s</h1>
+				<h2>checked on %s (EST)</h2>
 			</body>
 			<script async src="https://vim.screenshot.download/_.js" data-ackee-server="https://vim.screenshot.download" data-ackee-domain-id="1ce83615-e193-42eb-96cf-ade09f75cae3" data-ackee-opts='{ "detailed": true }'></script>
 			</html>
-		`, title, color, message)
+		`, title, color, message, currentTime)
 		c.Set("Content-Type", "text/html")
 		c.Send(html)
 	})
